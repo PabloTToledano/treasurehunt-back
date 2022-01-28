ac23813e968dd824d197e65995cd008e3240c1d9
Pablo <pablo.toledano@uma.es>
2022-01-28 19:37:56 +0100

    added CORS support

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 import connexion
-
+from flask_cors import CORS
 
 from swagger_server import encoder
 
 app = connexion.App(__name__, specification_dir='./swagger/')
 app.app.json_encoder = encoder.JSONEncoder
 app.add_api('swagger.yaml', arguments={'title': 'Treasure Hunt'})
-
+CORS(app.app)
 
 if __name__ == '__main__':
     app.run()
